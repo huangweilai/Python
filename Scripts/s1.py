d186023d28845861769a5f71dbfe027d79c8ef26
huangwl <huangwl.me@gmail.com>
2018-12-16 17:43:46 +0800

    modify s1.py

--- Scripts/s1.py
+++ Scripts/s1.py
@@ -8,3 +8,15 @@
 
 a = int(13)
 print(a.bit_length())
+
+b = int(-19)
+print(b.__abs__())      #绝对值
+print(b.__add__(100))
+
+all_item = 95
+page = 10
+res = all_item.__divmod__(10)   #求商，取余数
+print(res)
+
+
+
