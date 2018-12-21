bdca3370261f3b717a7f073b605db940828d1485
huangweilai <huangwl.me@gmail.com>
2018-12-21 20:40:13 +0800

    modify of_tuple.py

--- Scripts/of_tuple.py
+++ Scripts/of_tuple.py
@@ -2,12 +2,33 @@
 # @author=huangwl
 
 #多种方式定义元祖
+"""
 tu = (11,22,33,)
 tu = tuple((11,22,33,))
 tu = tuple([11,22,33,])
+"""
 #列表转换元祖
-""""
+"""
 li = [11,22,33,]
 tu = tuple(li)
 l = list(tu)
 """
+
+t = ('a','b',['A','B'])
+print(t)
+t[2][0] = 'X'
+t[2][1] = 'Y'
+print(t,'\n')
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+# 打印Apple:
+print(L[0][0])
+# 打印Python:
+print(L[1][1])
+# 打印Lisa:
+print(L[2][2])
+
