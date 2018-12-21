a57f84a310a76302e073fd328b44bb32d820dc68
huangweilai <huangwl.me@gmail.com>
2018-12-21 20:25:19 +0800

    add of_list.py

---
+++ Scripts/of_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# @author=huangwl
+
+
+li = list([1,2,3])
+# l2 = list((1,2,3))
+# print(li)
+li.append(99)
+# li.extend([11,22,33])       #扩展
+# li.extend((55,66,))         #扩展
+# li.insert(0,'huangwl')      #插入
+# ret = li.pop()                #删除一个
+# li.pop(0)                     #删除一个
+# li.remove(2)                  #删除指定的一个
+# li.reverse()                  #反转
+print(li)
