7c9a74a2f0b8a0c5c9edc2173fabdfd6b83cf030
huangweilai <huangwl.me@gmail.com>
2018-12-22 22:44:11 +0800

    add of_set.py

---
+++ Scripts/of_set.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# @author=huangwl
+
+
+# s1 = set()
+# s1.add('huangwl')
+# print(s1)
+
+s2 = set(['alex','eric','tony','alex',])
+print(s2)
+"""
+s3 = s2.difference(['alex','eric'])
+print(s2)
+print(s3)
+s4 = s2.difference_update(['alex','eric'])
+print(s2)
+print(s4)
+ret = s2.pop()
+print(s2)
+print(s2.remove('tony'))
+print(s2)
+"""
+
+
+
+
+
+
+
+
+
+
